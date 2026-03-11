05602f024d066368c0afc892aa957aa9ecfae2ae
Ogzygt28 <147761081+Ogzygt28@users.noreply.github.com>
2026-03-11 19:47:09 +0300

    Maven mimarisine geçildi ve bulut veritabanı entegrasyonu sağlandı

--- src/main/java/com/socialmedia/util/DBUtil.java
+++ src/main/java/com/socialmedia/util/DBUtil.java
@@ -4,9 +4,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBUtil {
-    private static final String URL = "jdbc:mysql://localhost:3306/social_media_app";
-    private static final String USER = "Kullanıcı Adın";
-    private static final String PASSWORD = "Şifren";
+    private static final String URL = "jdbc:mysql://mysql://ug7pr3pldqy48wti:7mmbBUrjEq1VagR2x8NI@bumany9xxetbkgcw7gl2-mysql.services.clever-cloud.com:3306/bumany9xxetbkgcw7gl2:3306/bumany9xxetbkgcw7gl2?useSSL=false&serverTimezone=UTC";;
+    private static final String USER = "ug7pr3pldqy48wti";
+    private static final String PASSWORD = "7mmbBUrjEq1VagR2x8NI";
 
     public static Connection getConnection() throws SQLException {
     try {
